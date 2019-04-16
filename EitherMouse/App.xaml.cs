8dfdf2e6179764f4b89aa30715089ef59aad7048
Tomas Voskovec <tomas.voskovec@gmail.com>
2019-04-16 15:51:33 +0200

    init

--- EitherMouse/App.xaml.cs
+++ EitherMouse/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 using System.Windows;
 
 namespace EitherMouse
@@ -11,7 +6,63 @@ namespace EitherMouse
     /// <summary>
     /// Interakční logika pro App.xaml
     /// </summary>
+
     public partial class App : Application
     {
+        private System.Windows.Forms.NotifyIcon _notifyIcon;
+        private bool _isExit;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow = new MainWindow();
+            MainWindow.Closing += MainWindow_Closing;
+
+            _notifyIcon = new System.Windows.Forms.NotifyIcon();
+            _notifyIcon.MouseClick += (s, args) => ShowMainWindow();
+            _notifyIcon.Icon = EitherMouse.Properties.Resources.Icon1;
+            _notifyIcon.Visible = true;
+
+            CreateContextMenu();
+        }
+
+        private void CreateContextMenu()
+        {
+            _notifyIcon.ContextMenuStrip =
+              new System.Windows.Forms.ContextMenuStrip();
+            _notifyIcon.ContextMenuStrip.Items.Add("EitherMouse").Click += (s, e) => ShowMainWindow();
+            _notifyIcon.ContextMenuStrip.Items.Add("Exit").Click += (s, e) => ExitApplication();
+        }
+
+        private void ExitApplication()
+        {
+            _isExit = true;
+            MainWindow.Close();
+            _notifyIcon.Dispose();
+            _notifyIcon = null;
+        }
+
+        public static Point GetMousePositionWindowsForms()
+        {
+            System.Drawing.Point point = System.Windows.Forms.Control.MousePosition;
+            return new Point(point.X, point.Y);
+        }
+
+
+        private void ShowMainWindow()
+        {
+            MainWindow.Top = GetMousePositionWindowsForms().Y - 400;
+            MainWindow.Left = GetMousePositionWindowsForms().X - 200;
+            MainWindow.Show();
+        }
+
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if (!_isExit)
+            {
+                e.Cancel = true;
+                MainWindow.Hide(); // A hidden window can be shown again, a closed one not
+            }
+        }
     }
 }
